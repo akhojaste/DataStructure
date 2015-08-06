71e0cc1f25d5b3da7ff5217a1a212313f596f94c
Amir Khojaste <amir.khojaste@sunnybrook.ca>
2015-08-06 16:34:16 -0400

    Binary Search Tree Data Structure

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+//-- BINARY SEARCH TREE
+
+class BstNode{
+public:
+	BstNode* left;
+	BstNode* right;
+	int data;
+	BstNode();
+	~BstNode();
+	int finMin(BstNode* root);
+	int finMax(BstNode* root);
+};
+
+BstNode::BstNode(){
+}
+
+BstNode::~BstNode(){
+}
+
+int BstNode::finMin(BstNode* root){
+
+	int min = 0;
+
+	if (root->left == NULL){ //-- we reached the end of the tree
+		return root->data;
+	}
+
+	else
+	{
+		while (root->left != NULL)
+		{
+			return this->finMin(root->left);
+		}
+	}
+}
+
+int BstNode::finMax(BstNode* root){
+
+	int min = 0;
+
+	if (root->right== NULL){ //-- we reached the end of the tree
+		return root->data;
+	}
+
+	else
+	{
+		while (root->right != NULL)
+		{
+			return this->finMin(root->right);
+		}
+	}
+}
+
+
+int main(int* argc, char* argv[])
+{
+  return 0;
+}
