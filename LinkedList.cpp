00555c9496e044aa02c2d128f38a676e0f527536
Amir Khojaste <amir.khojaste@sunnybrook.ca>
2015-08-06 16:31:03 -0400

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -12,7 +12,7 @@ node* ReverseLikedList(node* head)
 	node* temp = head;
 	node* nextNode = head;
 	node* prevNode = NULL;
-
+	
 	while (nextNode != NULL){ //-- while we did not reach the end of the list		
 		
 		nextNode = temp->next; // where the next node is
@@ -21,7 +21,7 @@ node* ReverseLikedList(node* head)
 		temp = nextNode;
 		
 	}
-
+	
 	head = prevNode;
 
 	return head;
@@ -30,28 +30,28 @@ node* ReverseLikedList(node* head)
 
 int main(int argc, char* argv[])
 {
-//-- n is pointer to a list member, for linked the lists member, we need a temporary pointer and also a pointer to the head of the list
-node* n = new node;
-n->data = 1;
-
-node* t = n; //-- temporary node
-node* head = n; //-- head of the list node
-
-n = new node;
-n->data = 2;
-t->next = n;
-t = t->next;
-
-n = new node;
-n->data = 3;
-n->next = NULL;
-
-t->next = n;
-t = t->next;
-
-//-- Reverse the linked list here, for this purpose, all you need is just the head pointer
-head = ReverseLikedList(head);
-
-return 0;
+	//-- n is pointer to a list member, for linked the lists member, we need a temporary pointer and also a pointer to the head of the list
+	node* n = new node;
+	n->data = 1;
+	
+	node* t = n; //-- temporary node
+	node* head = n; //-- head of the list node
+	
+	n = new node;
+	n->data = 2;
+	t->next = n;
+	t = t->next;
+	
+	n = new node;
+	n->data = 3;
+	n->next = NULL;
+	
+	t->next = n;
+	t = t->next;
+	
+	//-- Reverse the linked list here, for this purpose, all you need is just the head pointer
+	head = ReverseLikedList(head);
+	
+	return 0;
 
 }
